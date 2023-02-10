d95cadff97958ecc623c4fef18d04058fdffcd6d
shinnamie <shin.namie@rakus-partners.co.jp>
2023-02-10 17:55:41 +0900

    p78 francの掛け算のテストの削除

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -23,13 +23,6 @@ public class MoneyTest {
 	}
 	
 	@Test
-	public void Franc掛け算のテスト() {
-		Money five = Money.franc(5);
-		assertEquals(Money.franc(10),five.times(2));
-		assertEquals(Money.franc(15),five.times(3));
-	}
-	
-	@Test
 	public void 通貨クラスのテスト(){
 		assertEquals("USD", Money.dollar(1).currency());
 		assertEquals("CHF", Money.franc(1).currency());
