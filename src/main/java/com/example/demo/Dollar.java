813b6d4586fade20364852e8b59b37954fee4ee2
shinnamie <shin.namie@rakus-partners.co.jp>
2023-02-10 11:30:53 +0900

    chapter1-finished

--- src/main/java/com/example/demo/Dollar.java
+++ src/main/java/com/example/demo/Dollar.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
-import lombok.Data;
 
 public class Dollar {
 
-	int amount = 10;
+	int amount;
 
 	public Dollar(int amount) {
+		this.amount = amount;
 	}
 
 	public void times(int multiplier) {
+		amount *= multiplier;
 	}
 
+	
+	
 	public int getAmount() {
 		return amount;
 	}
