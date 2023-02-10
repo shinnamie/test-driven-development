83acd2b85ba800cf5c82d9d82f4085ad83a5d3c8
shinnamie <shin.namie@rakus-partners.co.jp>
2023-02-10 11:22:27 +0900

    chapter1-p8

---
+++ src/main/java/com/example/demo/Dollar.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import lombok.Data;
+
+public class Dollar {
+
+	int amount = 10;
+
+	public Dollar(int amount) {
+	}
+
+	public void times(int multiplier) {
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+
+}
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -3,16 +3,18 @@ package money;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.example.demo.Dollar;
+
 public class MoneyTest {
-	
+
 	@Test
 	public void testMultiplication() {
 		Dollar five = new Dollar(5);
-		five.time(2);
-		assertEquals(10, five.amounnt);
+
+		five.times(2);
+
+		assertEquals(10, five.getAmount());
 	}
 
 }
