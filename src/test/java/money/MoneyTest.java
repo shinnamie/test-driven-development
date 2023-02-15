7bc29fb474a80c212bbc4f4cc72460495e0b425a
shinnamie <shin.namie@rakus-partners.co.jp>
2023-02-15 10:47:47 +0900

    reduceメソッドのテストを記述

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -46,4 +46,12 @@ public class MoneyTest {
 		assertEquals(five, sum.addend);
 	}
 	
+	@Test
+	public void sumの計算結果を変形させるテスト(){
+		Expression sum = new Sum(Money.dollar(3),Money.dollar(4));
+		Bank bank = new Bank();
+		Money result = bank.reduce(sum, "USD");
+		assertEquals(Money.dollar(7),result);
+	}
+	
 }
