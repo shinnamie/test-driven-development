3e667614fb50d6651889cb1633ac3395850dba11
shinnamie <shin.namie@rakus-partners.co.jp>
2023-02-15 10:33:50 +0900

    sumクラスのテストが通った

--- src/main/java/money/Sum.java
+++ src/main/java/money/Sum.java
@@ -1,8 +1,14 @@
 package money;
 
-public class Sum {
+public class Sum implements Expression{
 	
 	Money augend;
 	Money addend;
+	
+	public Sum(Money augend, Money addend) {
+		this.augend = augend;
+		this.addend = addend;
+		
+	}
 
 }
